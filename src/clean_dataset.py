c50819aaa2781e25684d2781bee20af922e81c93
HermanDmukhouski <herman.dmukhouski@humandev.org>
2024-05-29 17:51:39 +0300

    fix: chnage connection to directories

--- src/clean_dataset.py
+++ src/clean_dataset.py
@@ -1,20 +1,14 @@
 import pandas as pd
 import os
-import sys 
 
 
 def clearing():
-
-    # Add src as folder from where to import
-    parent_dir = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), '.', 'data'))
-
-    # Add this directory to sys.path
-    sys.path.append(parent_dir)
-
+    base_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+    save_dir = os.path.join(base_dir, 'data')
 
     # Load the .parquet files
-    df1 = pd.read_parquet(r'../data/train.parquet')
-    df2 = pd.read_parquet(r'../data/test.parquet')
+    df1 = pd.read_parquet(os.path.join(save_dir, 'train.parquet'))
+    df2 = pd.read_parquet(os.path.join(save_dir, 'test.parquet'))
 
     # Concatenate them into one DataFrame
     combined_df = pd.concat([df1, df2])
@@ -32,8 +26,10 @@ def clearing():
     clean_df = clean_df[~duplicates]
 
     # Save the cleaned DataFrame to a new .parquet file
-    clean_df.to_parquet(r'../data/clean_data.parquet')
+    clean_df.to_parquet(os.path.join(save_dir, 'clean_data.parquet'))
 
     # Print the number of rows before and after cleaning
     print(f"Initial number of rows: {len(combined_df)}")
     print(f"Number of rows after removing nulls and duplicates: {len(clean_df)}")
+
+
